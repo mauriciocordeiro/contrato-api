e0c0e37a3c67ef52f3ba4675f699227a2e300a3a
Maurício Cordeiro <mauriciocordeiro@live.com>
2020-11-21 15:54:55 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,51 @@
 let express = require('express'),
-    path = require('path'),
     mongoose = require('mongoose'),
     cors = require('cors'),
     bodyParser = require('body-parser'),
     dataBaseConfig = require('./database/db')
 
-// MongoDB ====================================================================
+const app = express()
+
+// DATABASE ===================================================================
 mongoose.Promise = global.Promise
 mongoose.connect(dataBaseConfig.db, {
     useNewUrlParser: true
 }).then(() => {
-    console.log('BD conectado!')
+    console.log('Database is up!')
 },
     error => {
-        console.log('Não foi possível estabelecer conexão om o BD: ' + error)
+        console.log('Cannot connect database: ' + error)
     }
 )
+// ============================================================================
 
 // ROUTES =====================================================================
-const app = express()
+// contrato -------------------------------------------------------------------
+const contratoController = require('./controller/contrato.controller')
+app.use('/contratos', contratoController)
+// ----------------------------------------------------------------------------
+// empresa --------------------------------------------------------------------
+const empresaController = require('./controller/empresa.controller')
+app.use('/empresas', empresaController)
+// ----------------------------------------------------------------------------
+// modelo ---------------------------------------------------------------------
+const modelController = require('./controller/model.controller')
+app.use('/model', modelController)
+// ----------------------------------------------------------------------------
+// ============================================================================
+
+// ERROR HANDLER ==============================================================
+app.use(function (err, req, res, next) {
+    console.error(err.message)
+    if (!err.statusCode) err.statusCode = 500
+    res.status(err.statusCode).send(err.message)
+})
+// ============================================================================
+
+// SERVER =====================================================================
+const server = app.listen(3000, () => {
+    console.log('Server is up!')
+})
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
     extended: false
@@ -29,36 +56,4 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     next()
 })
-
-// contrato -------------------------------------------------------------------
-const contratoController = require('./controller/contrato.controller')
-app.use('/contratos', contratoController)
-// ----------------------------------------------------------------------------
-// empresa --------------------------------------------------------------------
-const empresaController = require('./controller/empresa.controller')
-app.use('/empresas', empresaController)
-// ----------------------------------------------------------------------------
-
-// modelo ---------------------------------------------------------------------
-const modelController = require('./controller/model.controller')
-app.use('/model', modelController)
-// ----------------------------------------------------------------------------
 // ============================================================================
-
-// Create port
-const port = 3000
-const server = app.listen(port, () => {
-    console.log('Connected to port ' + port)
-})
-
-// Find 404 and hand over to error handler
-app.use((req, res, next) => {
-    next(createError(404))
-})
-
-// error handler
-app.use(function (err, req, res, next) {
-    console.error(err.message)
-    if (!err.statusCode) err.statusCode = 500
-    res.status(err.statusCode).send(err.message)
-})
